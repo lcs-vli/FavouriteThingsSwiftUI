5c8ab5c1c95c568bca361c9bd6283e90cafcb709
Victoria Li <VLi@lcs.on.ca>
2021-01-08 14:29:53 -0500

    Initial Commit

---
+++ FavouriteThingsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteThingsSwiftUI
+//
+//  Created by Li, Muchen on 2021/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FavouriteThingsSwiftUI/FavouriteThingsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsSwiftUIApp.swift
+//  FavouriteThingsSwiftUI
+//
+//  Created by Li, Muchen on 2021/1/8.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
