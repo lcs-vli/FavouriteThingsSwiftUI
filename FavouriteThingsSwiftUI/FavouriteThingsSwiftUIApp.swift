4e6e20eed011a2cbdfe87cde1003bc611d7232e1
Victoria Li <VLi@lcs.on.ca>
2021-01-09 20:08:13 -0500

    Created an Icecreamview and added an image of ice cream and description

--- FavouriteThingsSwiftUI/FavouriteThingsSwiftUIApp.swift
+++ FavouriteThingsSwiftUI/FavouriteThingsSwiftUIApp.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct FavouriteThingsSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
+            NavigationView{
+                ContentView()
+            }
+            
         }
     }
 }
